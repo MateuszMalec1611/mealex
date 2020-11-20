a39433358feddeb70ebcc82e31325c7a479b359f
Mateusz Malec <m.mateusz2000@wp.pl>
2020-11-20 15:57:09 +0100

    added corrections

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
 const burgerBtn = document.querySelector('.burger-btn');
 const burgerBtnOpen = document.querySelector('.burger-btn__open');
 const burgerBtnclose = document.querySelector('.burger-btn__close');
 const navMobile = document.querySelector('.nav__box');
 const allNavItems = document.querySelectorAll ('.nav__links-item');
 
-
-function handleClick() {
+    function handleClick() {
     navMobile.classList.toggle('nav__box--active');
     burgerBtnOpen.classList.toggle('burger-btn__open--close');
     burgerBtnclose.classList.toggle('burger-btn__close--open');
@@ -13,3 +15,4 @@ function handleClick() {
 }
 
 burgerBtn.addEventListener('click', handleClick);
+});
