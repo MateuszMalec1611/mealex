e8bc9808620af4e51bfca8eddd34f0c1720957cd
Mateusz Malec <m.mateusz2000@wp.pl>
2020-11-23 20:07:52 +0100

    added shadow to nav

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-
-const burgerBtn = document.querySelector('.burger-btn');
-const burgerBtnOpen = document.querySelector('.burger-btn__open');
-const burgerBtnclose = document.querySelector('.burger-btn__close');
-const navMobile = document.querySelector('.nav__box');
-const allNavItems = document.querySelectorAll ('.nav__links-item');
+    const nav = document.querySelector('.nav')
+    const burgerBtn = document.querySelector('.burger-btn');
+    const burgerBtnOpen = document.querySelector('.burger-btn__open');
+    const burgerBtnclose = document.querySelector('.burger-btn__close');
+    const navMobile = document.querySelector('.nav__box');
 
     function handleClick() {
-    navMobile.classList.toggle('nav__box--active');
-    burgerBtnOpen.classList.toggle('burger-btn__open--close');
-    burgerBtnclose.classList.toggle('burger-btn__close--open');
+        navMobile.classList.toggle('nav__box--active');
+        burgerBtnOpen.classList.toggle('burger-btn__open--close');
+        burgerBtnclose.classList.toggle('burger-btn__close--open');
 
-}
+    }
+    burgerBtn.addEventListener('click', handleClick);
 
-burgerBtn.addEventListener('click', handleClick);
+
+
+    function addShadow() {
+        if (window.scrollY >= 39) {
+            nav.classList.add('nav__shadow')
+        } else {
+            nav.classList.remove('nav__shadow')
+        }
+    }
+    window.addEventListener('scroll', addShadow)
 });
