a94c222bbb928330858967c2d0909251013250a5
Mateusz Malec <m.mateusz2000@wp.pl>
2020-11-23 20:49:35 +0100

    added correction to nav shadow

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const nav = document.querySelector('.nav')
+    const nav = document.querySelector('.nav');
+    const navBox = document.querySelector('.nav__box');
     const burgerBtn = document.querySelector('.burger-btn');
     const burgerBtnOpen = document.querySelector('.burger-btn__open');
     const burgerBtnclose = document.querySelector('.burger-btn__close');
@@ -18,8 +19,10 @@ document.addEventListener('DOMContentLoaded', function () {
     function addShadow() {
         if (window.scrollY >= 39) {
             nav.classList.add('nav__shadow')
+            navBox.classList.add('nav__box-margin')
         } else {
             nav.classList.remove('nav__shadow')
+            navBox.classList.remove('nav__box-margin')
         }
     }
     window.addEventListener('scroll', addShadow)
